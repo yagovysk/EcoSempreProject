b11aac75627df4ebf417ddc5eee60dab468548f2
yrllanbrandao <yrllanbrandao@outlook.com>
2023-05-08 07:55:37 -0300

    config: database config

---
+++ server/database/connection.ts
@@ -0,0 +1,22 @@
+import knex, {Knex} from 'knex';
+import dotenv from 'dotenv';
+dotenv.config();
+const host:string = "172.17.0.2";
+const port:number = 3306;
+const user:string = "root";
+const database:string = "ecoSempre";
+const knexConfig: Knex.Config = {
+    client: "mysql2",
+    connection:{
+        host,
+        port,
+        user,
+        password: process.env.DB_PASSWORD,
+        database
+    }
+}
+
+const Connection:unknown = knex(knexConfig);
+
+
+export default Connection;
