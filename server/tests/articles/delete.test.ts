c71ec6ac3f0404438fd92a858ed1380e9433abd3
yrllanbrandao <yrllanbrandao@outlook.com>
2023-05-14 09:35:52 -0300

    feat(articles): get all articles

--- server/tests/articles/delete.test.ts
+++ server/tests/articles/delete.test.ts
@@ -2,24 +2,18 @@ import {it, describe, test, expect} from '@jest/globals';
 import supertest, {Response} from "supertest";
 import app from '../../server';
 
-
-
-
-
-
-
 describe("DELETE /article/id", () =>{
     
     const id:number = 3;
     it("it should returns 404", async () =>{
         const res: Response = await supertest(app)
-        .delete(`/articles/${id}`);
+        .delete(`/article/${id}`);
         expect(res.status).toBe(200);
     })
     it.only("it should returns 404", async () =>{
    
         const res: Response = await supertest(app)
-        .delete(`/articles/${id}`);
+        .delete(`/article/${id}`);
         expect(res.status).toBe(404);
     })
 })
