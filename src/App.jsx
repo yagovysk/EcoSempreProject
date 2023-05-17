57868a12e364e86c3affe1e45f51a26638001672
Yago Cerqueira Regis <45441676+yagovysk@users.noreply.github.com>
2023-05-17 00:33:58 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from './components/Header.jsx';
+import { Home } from './components/Home.jsx';
 import './global.css'
 
 export function App() {
   return (
     <div>
     <Header />
+    <Home />
     </div>
   )
 }
