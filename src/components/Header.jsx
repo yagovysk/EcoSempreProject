2ad18ce30f517dfe760346f98b922d4b53dd3014
Yago Cerqueira Regis <45441676+yagovysk@users.noreply.github.com>
2023-05-17 00:33:58 -0300

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 import logo from '../assets/logoEcoSempre.png';
 import styles from './Header.module.css';
+import lupa from '../assets/lupaicon.png';
+
 
 export function Header() {
     return(
     <header className={ styles.header }>
         <img className={styles.logo} src={ logo } alt="logo" />
+        <ul className={styles.menu}>
+            <li><a href="#"> Início </a></li>
+            <li><a href="#"> EcoSempre ⌄</a></li>
+            <li><a href="#">Programas ⌄</a></li>
+            <li><a href="#">Pontos de coleta</a></li>
+            <li><a href="#">Blog</a></li>
+        </ul>
+        <img className={styles.lupaicon} src={ lupa } alt="iconlupa" />
+        <a className={ styles.contato } href="#">Entre em contato <span> 🡢 </span></a>
     </header>
   );
 }
