e1e7071bc6188fecd9ff3b20c83b7287db228968
yrllanbrandao <yrllanbrandao@outlook.com>
2023-05-18 18:21:46 -0300

    feat(article): find article by ID from endpoint /article/[id]

---
+++ server/tests/articles/getById.test.ts
@@ -0,0 +1,27 @@
+import {it, describe, expect} from '@jest/globals';
+import supertest, {Response} from 'supertest';
+import app from '../../server';
+
+
+
+
+
+describe("GET /articles/[id]", ()=>{
+
+    it("it should returns 404 status", async () =>{
+
+        const res:Response = await supertest(app)
+        .get("/article/4");
+
+        expect(res.status).toBe(404);
+    })
+    it("it should returns 200 status", async () =>{
+
+        const res:Response = await supertest(app)
+        .get("/article/5");
+
+        expect(res.status).toBe(200);
+
+        
+    })
+})
