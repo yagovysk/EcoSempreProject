9e72813a7463906c36397db51d9ef44c1c08fe94
Yago Cerqueira Regis <45441676+yagovysk@users.noreply.github.com>
2023-05-18 15:52:47 -0300

    Add files via upload

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -4,7 +4,23 @@ import sobreimg from '../assets/img.svg'
 export function Sobre() {
     return(
         <div className={styles.sobre}>
-            <img src={sobreimg} alt="" />
+            <img src={sobreimg} alt="imagem ilustrativa" />
+            <article className={styles.text}>
+                <h1> Nossa História 
+                    e Compromisso 
+                    Sustentável 
+                </h1>
+                <h2> 
+                    Somos Pioneiros em Sustentabilidade 
+                    e Logística Reversa de Eletrônicos! 
+                </h2>
+                <p> 
+                    Trabalhamos incansavelmente para desenvolver 
+                    soluções inovadoras em Logística Reversa e
+                    ajudar a transformar a maneira como as empresas 
+                    lidam com seus resíduos.
+                </p>
+            </article>
         </div>
     )
 }
