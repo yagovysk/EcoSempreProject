f639f7ea7769cd8cea3e65b427a86222b49e453a
Raiane <raiane.oliveira404@gmail.com>
2023-05-23 15:37:07 -0300

    feat: increase number of garbage dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
-import { Header } from './components/Header.jsx';
+import { Header } from "./components/Header.jsx";
 
-import { Home } from './components/Home.jsx';
+import { Home } from "./components/Home.jsx";
 
-import { Sobre } from './components/Sobre.jsx';
+import { Sobre } from "./components/Sobre.jsx";
 
-import { Sustentabilidade } from './components/Sustentabilidade.jsx';
+import { Sustentabilidade } from "./components/Sustentabilidade.jsx";
 
-import { Saibaquais } from "./components/Saiba.jsx";
+import { Saiba } from "./components/Saiba.jsx";
 
+import style from "./App.module.css";
 
-import style from './App.module.css';
-
-import './global.css'
+import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <section className={ style.idhome }>
+      <section className={style.idhome}>
         <Home />
-      </section> 
-      <section className={ style.idsobre }>
+      </section>
+      <section className={style.idsobre}>
         <Sobre />
       </section>
-      <section className={ style.idsustentabilidade }>
+      <section className={style.idsustentabilidade}>
         <Sustentabilidade />
       </section>
       <section className={style.idSaibaquais}>
-        <Saibaquais />
+        <Saiba />
       </section>
     </div>
-  )
+  );
 }
-
-
--- src/components/Saiba.jsx
+++ src/components/Saiba.jsx
@@ -1,56 +1,60 @@
-import styles from './Saiba.module.css';
+import styles from "./Saiba.module.css";
 import smartPhoneImg from "../assets/smartphone.svg";
 import computadorImg from "../assets/computador.svg";
 import impressoraImg from "../assets/impressora.svg";
 import cameraImg from "../assets/camera.svg";
 
-
 export function Saiba() {
-    return (
-        <div className={styles.box}>
-            <article className={styles.oQuePode}>
-                <h1>O que pode ser descartado?</h1>
-                <h2>
-                    Saiba Quais são os Tipos
-                    de Lixo Eletrônico que Podem
-                    ser Coletados
-                </h2>
-                <p>
-                    Lembre-se de realizar o descarte correto para proteger o meio ambiente e evitar danos à saúde pública. Em nossos pontos de coleta, aceitamos diversos tipos de lixo eletrônico e eletroeletrônico, desde smartphones antigos, até pilhas, baterias e eletrodomésticos.
-                </p>
-                <button>Ver Lista Completa</button>
-            </article>
-            <article className={styles.dispositivos}>
-                <div className={styles.smartphones}>
-                    <img src={smartPhoneImg} alt={"Smartphone"} />
-                    <h1>Smartphones</h1>
-                    <p> Atualize seus dispositivos eletrônicos
-                        e descarte os antigos com segurança
-                        e responsabilidade.</p>
-                </div>
-                <div className={styles.computadores}>
-                    <img src={computadorImg} alt={"Computador"} />
-                    <h1>Computadores</h1>
-                    <p>
-                        Ao substituir seu equipamento antigo, você pode dar a ele uma segunda vida ao doar ou reciclar de maneira responsável.
-                    </p>
-                </div>
-                <div className={styles.impressoras}>
-                    <img src={impressoraImg} alt={"Impressora"} />
-                    <h1>Impressoras</h1>
-                    <p>
-                        Recicle com responsabilidade, garanta que materiais como cartuchos de tinta e toners sejam descartados corretamente.
-                    </p>
-                </div>
-                <div className={styles.cameras}>
-                    <img src={cameraImg} alt={"Camera"} />
-                    <h1>Câmeras</h1>
-                    <p>
-                        Recicle corretamente para evitar que ela se torne lixo eletrônico e contribuir para a preservação do meio ambiente.
-                    </p>
-                </div>
-            </article>
+  return (
+    <div className={styles.box}>
+      <article className={styles.oQuePode}>
+        <h1>O que pode ser descartado?</h1>
+        <h2>
+          Saiba Quais são os Tipos de Lixo Eletrônico que Podem ser Coletados
+        </h2>
+        <p>
+          Lembre-se de realizar o descarte correto para proteger o meio ambiente
+          e evitar danos à saúde pública. Em nossos pontos de coleta, aceitamos
+          diversos tipos de lixo eletrônico e eletroeletrônico, desde
+          smartphones antigos, até pilhas, baterias e eletrodomésticos.
+        </p>
+        <button>Ver Lista Completa</button>
+      </article>
+      <article className={styles.dispositivos}>
+        <div className={styles.smartphones}>
+          <img src={smartPhoneImg} alt={"Smartphone"} />
+          <h1>Smartphones</h1>
+          <p>
+            {" "}
+            Atualize seus dispositivos eletrônicos e descarte os antigos com
+            segurança e responsabilidade.
+          </p>
         </div>
-    );
+        <div className={styles.computadores}>
+          <img src={computadorImg} alt={"Computador"} />
+          <h1>Computadores</h1>
+          <p>
+            Ao substituir seu equipamento antigo, você pode dar a ele uma
+            segunda vida ao doar ou reciclar de maneira responsável.
+          </p>
+        </div>
+        <div className={styles.impressoras}>
+          <img src={impressoraImg} alt={"Impressora"} />
+          <h1>Impressoras</h1>
+          <p>
+            Recicle com responsabilidade, garanta que materiais como cartuchos
+            de tinta e toners sejam descartados corretamente.
+          </p>
+        </div>
+        <div className={styles.cameras}>
+          <img src={cameraImg} alt={"Camera"} />
+          <h1>Câmeras</h1>
+          <p>
+            Recicle corretamente para evitar que ela se torne lixo eletrônico e
+            contribuir para a preservação do meio ambiente.
+          </p>
+        </div>
+      </article>
+    </div>
+  );
 }
-
