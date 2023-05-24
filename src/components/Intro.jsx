11df11ed6cee35bb628dba50cce9fea27a5b6932
Raiane <raiane.oliveira404@gmail.com>
2023-05-24 14:47:41 -0300

    feat: add routes

---
+++ src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import styles from "./Intro.module.css";
+
+export function Intro() {
+  return (
+    <div className={styles.home}>
+      <nav className={styles.hometitle}>
+        <h1>Logística Reversa de Eletrônicos</h1>
+      </nav>
+      <nav className={styles.homeparagraph}>
+        <p>
+          Trabalhamos em conjunto com comunidades e parceiros para desenvolver
+          soluções inovadoras em Logística Reversa.
+        </p>
+        <a href="#">
+          Saiba Mais
+          <span>🡢</span>
+        </a>
+      </nav>
+    </div>
+  );
+}
