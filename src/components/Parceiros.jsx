06bc297324cbde6dfe47a05728f0b7ea2b5fa41f
Raiane <raiane.oliveira404@gmail.com>
2023-05-25 16:25:25 -0300

    refactor: parceiros section

--- src/components/Parceiros.jsx
+++ src/components/Parceiros.jsx
@@ -3,27 +3,45 @@ import Parceiro2 from "../assets/Parceiro2.svg";
 import Parceiro3 from "../assets/Parceiro3.svg";
 import Parceiro4 from "../assets/Parceiro4.svg";
 import Parceiro5 from "../assets/Parceiro5.svg";
-import style from "../components/Parceiros.module.css";
+import styles from "../components/Parceiros.module.css";
 
-export function Parceiros(){
-    return(
-        <section className={ style.idParceiros }>
-            <article>
-                <h1>
-                    Nossos Parceiros
-                </h1>
-                <h2>
-                    Conheça Quem Confia na
-                    EcoSempre
-                </h2>
-            </article>
-            <div className={style.parceirosCard}>
-            <img className={ style.img1 } src={ Parceiro1 } alt="logo parceiro 1" />
-            <img className={ style.img2 } src={ Parceiro2 } alt="logo parceiro 2" />
-            <img className={ style.img3 } src={ Parceiro3 } alt="logo parceiro 3" />
-            <img className={ style.img4 } src={ Parceiro4 } alt="logo parceiro 4" />
-            <img className={ style.img5 } src={ Parceiro5 } alt="logo parceiro 5" />
-            </div>
-        </section>
-    )
+export function Parceiros() {
+  return (
+    <article className={styles.idParceiros}>
+      <section className={`container ${styles.content}`}>
+        <div className={styles.texts}>
+          <span className={`small-text`}>Nossos Parceiros</span>
+          <h2 className={`title`}>Conheça Quem Confia na EcoSempre</h2>
+        </div>
+
+        <div className={styles.parceirosCard}>
+          <img
+            className={styles.parceiro}
+            src={Parceiro1}
+            alt="logo parceiro 1"
+          />
+          <img
+            className={styles.parceiro}
+            src={Parceiro2}
+            alt="logo parceiro 2"
+          />
+          <img
+            className={styles.parceiro}
+            src={Parceiro3}
+            alt="logo parceiro 3"
+          />
+          <img
+            className={styles.parceiro}
+            src={Parceiro4}
+            alt="logo parceiro 4"
+          />
+          <img
+            className={styles.parceiro}
+            src={Parceiro5}
+            alt="logo parceiro 5"
+          />
+        </div>
+      </section>
+    </article>
+  );
 }
