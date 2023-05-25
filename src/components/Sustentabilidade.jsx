922299c79ae63bfb49cbdc5672f24037ae77db59
Raiane <raiane.oliveira404@gmail.com>
2023-05-25 14:21:28 -0300

    fix: sustentabilidade section styles

--- src/components/Sustentabilidade.jsx
+++ src/components/Sustentabilidade.jsx
@@ -5,22 +5,24 @@ export function Sustentabilidade() {
   return (
     <div className={styles.sustentabilidadeBox}>
       <img src={sustentabilidadeImg} className={styles.img} />
-      <article className={styles.text}>
-        <h1 className={styles.sustentabilidadeTitle}>Sustentabilidade</h1>
-        <h2 className={styles.porque}>
-          Por que é tão Importante Realizar o Descarte Correto do Lixo
-          Eletrônico?
-        </h2>
-        <p className={styles.sustentabilidadeP}>
-          O lixo eletrônico é um dos maiores desafios ambientais da nossa era.
-          Nós acreditamos que a conscientização é o primeiro passo para uma
-          solução sustentável. Junte-se a nós nessa jornada e faça a sua parte
-          para um mundo mais limpo e saudável.
-        </p>
-        <a href="#" className={styles.saibamais}>
-          Saiba Mais
-          <span>🡢</span>
-        </a>
+      <article className={styles.textsContainer}>
+        <span className={`small-text`}>Sustentabilidade</span>
+        <section className={styles.texts}>
+          <h2 className={styles.porque}>
+            Por que é tão Importante Realizar o Descarte Correto do Lixo
+            Eletrônico?
+          </h2>
+          <p className={styles.sustentabilidadeP}>
+            O lixo eletrônico é um dos maiores desafios ambientais da nossa era.
+            Nós acreditamos que a conscientização é o primeiro passo para uma
+            solução sustentável. Junte-se a nós nessa jornada e faça a sua parte
+            para um mundo mais limpo e saudável.
+          </p>
+          <button className={`btn ${styles.saibamais}`} type="button">
+            <a href="#">Saiba Mais</a>
+            <span>🡢</span>
+          </button>
+        </section>
       </article>
     </div>
   );
