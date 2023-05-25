7a3644dbec27235d04c3373ad6cb146828a8c940
Yago Cerqueira Regis <45441676+yagovysk@users.noreply.github.com>
2023-05-25 16:43:43 -0300

    Create Depoiments.jsx

---
+++ src/components/Depoiments.jsx
@@ -0,0 +1,24 @@
+import style from "../components/Depoiments.module.css";
+
+export function Depoiments() {
+    return(
+        <section className={ style.idDepoiments }>
+            <article>
+                <h1>
+                    Experiências Inspiradoras
+                </h1>
+                <h2>
+                    Depoimentos Daqueles
+                    que já Passaram na
+                    EcoSempre
+                </h2>
+                <a href="#">
+                    Sobre Nós
+                    <span>
+                        🡢
+                    </span>
+                </a>
+            </article>
+        </section>
+    )
+}
