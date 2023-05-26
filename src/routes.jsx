a70a669c97e54a7bee32d77ed0eb2163a6845d7b
Raiane <raiane.oliveira404@gmail.com>
2023-05-26 15:40:20 -0300

    feat: about us page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Default } from "./layouts/Default";
+import { AboutUs } from "./pages/AboutUs";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/about_us",
+        element: <AboutUs />,
       },
     ],
   },
