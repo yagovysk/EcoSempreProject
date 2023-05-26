43173d0a4a3b8aae05a91588394d76fe232332ba
Raiane <raiane.oliveira404@gmail.com>
2023-05-26 18:27:39 -0300

    feat: footer component

--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 export function Default() {
   return (
@@ -8,6 +9,7 @@ export function Default() {
         <Header />
       </div>
       <Outlet />
+      <Footer />
     </>
   );
 }
