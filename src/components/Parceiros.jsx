0ebaac82ae8bbc0ebffb292f6717c7724e06d291
Raiane <raiane.oliveira404@gmail.com>
2023-05-26 15:40:20 -0300

    feat: about us page

--- src/components/Parceiros.jsx
+++ src/components/Parceiros.jsx
@@ -1,9 +1,5 @@
-import Parceiro1 from "../assets/Parceiro1.svg";
-import Parceiro2 from "../assets/Parceiro2.svg";
-import Parceiro3 from "../assets/Parceiro3.svg";
-import Parceiro4 from "../assets/Parceiro4.svg";
-import Parceiro5 from "../assets/Parceiro5.svg";
 import styles from "../components/Parceiros.module.css";
+import { ParceirosCard } from "./ParceirosCard";
 
 export function Parceiros() {
   return (
@@ -14,33 +10,7 @@ export function Parceiros() {
           <h2 className={`title`}>Conheça Quem Confia na EcoSempre</h2>
         </div>
 
-        <div className={styles.parceirosCard}>
-          <img
-            className={styles.parceiro}
-            src={Parceiro1}
-            alt="logo parceiro 1"
-          />
-          <img
-            className={styles.parceiro}
-            src={Parceiro2}
-            alt="logo parceiro 2"
-          />
-          <img
-            className={styles.parceiro}
-            src={Parceiro3}
-            alt="logo parceiro 3"
-          />
-          <img
-            className={styles.parceiro}
-            src={Parceiro4}
-            alt="logo parceiro 4"
-          />
-          <img
-            className={styles.parceiro}
-            src={Parceiro5}
-            alt="logo parceiro 5"
-          />
-        </div>
+        <ParceirosCard />
       </section>
     </article>
   );
