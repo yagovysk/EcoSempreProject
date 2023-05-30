f5cd2e32457f5af7026ae570da1bee0423d7888d
Raiane <raiane.oliveira404@gmail.com>
2023-05-30 17:08:07 -0300

    fix: Callwithus component style

--- src/components/Home/Callwithus.jsx
+++ src/components/Home/Callwithus.jsx
@@ -1,40 +1,30 @@
 import styles from "../Home/Callwithus.module.css";
+import { FormTalkWithUs } from "../FormTalkWithUs";
 
 export function Callwithus() {
-    return(
-        <section className={ styles.idfaleconosco }>
-            <nav className={styles.cardFale}>
-                <article>
-                    <h1>
-                        Fale conosco!
-                    </h1>
-                    <p>
-                        Entre em contato
-                        conosco por meio
-                        do formulário abaixo.
-                        Estamos disponíveis 
-                        para responder suas
-                        dúvidas, fornecer 
-                        informações e receber 
-                        suas sugestões. 
-                    </p>
-                </article>
-                <form className={ styles.FormCall }>
-                    <ul className={ styles.FormList1 }>
-                        <li>
-                            <input type="text" placeholder="Nome" />
-                        </li>
-                        <li>
-                            <input type="tel" placeholder="Telefone" />
-                        </li>
-                    </ul>
-                    <ul className={ styles.FormList2 }>
-                        <li>
-                            <input type="text" placeholder="E-mail" />
-                        </li>
-                    </ul>
-                </form>
-            </nav>
-        </section>
-    )
+  return (
+    <div className={styles.idfaleconosco}>
+      <div className={`container ${styles.wrapper_content}`}>
+        <div className={styles.wrapper_form}>
+          <FormTalkWithUs />
+        </div>
+
+        <div className="textsContainer">
+          <span className="small-text">Atendimento Personalizado</span>
+          <div className="texts">
+            <h2 className={styles.title}>
+              Nós da EcoSempre Estaremos Felizes em Atender Você!
+            </h2>
+            <p className={styles.description}>
+              Queremos proporcionar a melhor experiência possível. Preencha o
+              formulário ao lado com suas dúvidas,informações, solicitações ou
+              feedback. Estamos prontos para ajudar e oferecer soluções
+              personalizadas. Sua satisfação é nossa prioridade e estamos
+              ansiosos para atender você!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
