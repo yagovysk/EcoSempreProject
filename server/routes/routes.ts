fe66f09a551add11a627a31480918664d046c808
yrllanbrandao <yrllanbrandao@outlook.com>
2023-05-30 11:20:05 -0300

    feat: register contact route

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,46 +1,55 @@
-import express, { Router, Request, Response,NextFunction }  from 'express';
+import express, { Router, Request, Response, NextFunction } from 'express';
 import User from '../models/user';
 import Article from '../models/article';
+import Contact from '../models/contact';
 import Middleware from '../auth/middleware';
-const Routes:Router = express.Router();
+const Routes: Router = express.Router();
 
 
 
 
-Routes.post("/authentication",   async (req:Request, res:Response) =>{
+Routes.post("/authentication", async (req: Request, res: Response) => {
 
-    const user:User = new User();
+    const user: User = new User();
 
     user.login(req, res);
 
 })
-Routes.post("/user", Middleware, async(req:Request, res:Response)=>{
-    const user:User = new User();
+Routes.post("/user", Middleware, async (req: Request, res: Response) => {
+    const user: User = new User();
     user.createUser(req, res);
 })
 
+// contact
+
+Routes.post("/contact", async (req: Request, res: Response) => {
+    const contact: Contact = new Contact();
+
+    contact.registerContact(req, res);
+})
 
 // articles
-Routes.get("/articles", Middleware, async(req:Request, res:Response)=>{
-    const article:Article = new Article();
+Routes.get("/articles", Middleware, async (req: Request, res: Response) => {
+    const article: Article = new Article();
     article.getArticles(req, res);
 })
-Routes.post("/article",Middleware, async(req:Request, res:Response)=>{
-    const article:Article = new Article();
+Routes.post("/article", Middleware, async (req: Request, res: Response) => {
+    const article: Article = new Article();
     article.creatArticle(req, res);
-    
+
 })
 
-Routes.get("/article/:key", Middleware,  async(req:Request, res:Response) =>{
-    const article:Article = new Article();
+Routes.get("/article/:key", Middleware, async (req: Request, res: Response) => {
+    const article: Article = new Article();
     article.getArticleByKey(req, res);
 })
-Routes.delete("/article/:id", Middleware, async(req:Request, res:Response) =>{
-    const article:Article = new Article();
+Routes.delete("/article/:id", Middleware, async (req: Request, res: Response) => {
+    const article: Article = new Article();
     article.deleteArticle(req, res);
 })
-Routes.put("/article/:id", Middleware, async(req:Request, res:Response) =>{
-    const article:Article = new Article();
+Routes.put("/article/:id", Middleware, async (req: Request, res: Response) => {
+    const article: Article = new Article();
     article.updateArticle(req, res);
 })
+
 export default Routes;
