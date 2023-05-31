827c61974347096e0353f0a611b7c7b9c213ed5c
Raiane <raiane.oliveira404@gmail.com>
2023-05-31 16:49:20 -0300

    fix: transition duration of card blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,23 +2,23 @@ import { HeaderSection } from "../components/HeaderSection.jsx";
 import styles from "../pages/Blog.module.css";
 
 const linksMenu = [
-    {
-       name: "Home",
-        path: "/"
-    },
-    {
-      name: "Blog"
-    }
-]
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Blog",
+  },
+];
 
 export function Blog() {
-    return(
-        <main>
-            <HeaderSection
-                className={ styles.header }
-                title="Nosso Blog"
-                linksMenu={ linksMenu }
-             />
-        </main>
-    )
+  return (
+    <main>
+      <HeaderSection
+        className={styles.header}
+        title="Nosso Blog"
+        linksMenu={linksMenu}
+      />
+    </main>
+  );
 }
