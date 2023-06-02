326affec2f1051eb269bf34311c1cd23319d11ff
yrllanbrandao <yrllanbrandao@outlook.com>
2023-06-02 10:10:07 -0300

    feat(contacts):  pagination

--- server/dist/models/contact.js
+++ server/dist/models/contact.js
@@ -17,11 +17,31 @@ const static_1 = __importDefault(require("../static"));
 class Contact {
     constructor() {
         this.currentDate = new static_1.default().getCurrentDate();
+        this.verifyPagination = (page, limit) => {
+            if (limit === undefined || page === undefined) {
+                return false;
+            }
+            return true;
+        };
         this.findAll = (req, res) => __awaiter(this, void 0, void 0, function* () {
             try {
-                const contacts = yield (0, connection_1.default)("contacts").select("*");
-                yield connection_1.default.destroy();
-                res.status(200).send(contacts);
+                const { limit, page } = req.query;
+                const pagination = this.verifyPagination(limit, page);
+                if (pagination) {
+                    const offset = (Number(page) - 1) * Number(limit);
+                    const contacts = yield (0, connection_1.default)("contacts").select("*").limit(Number(limit)).offset(offset);
+                    if (contacts[0] === undefined) {
+                        res.status(404).send("doesn't exists contacts");
+                    }
+                    else {
+                        res.status(200).send(contacts);
+                    }
+                }
+                else {
+                    const contacts = yield (0, connection_1.default)("contacts").select("*");
+                    yield connection_1.default.destroy();
+                    res.status(200).send(contacts);
+                }
             }
             catch (error) {
                 res.sendStatus(400);
