c187f1b6738fbb521e4c476d9679d7b3254fc76b
yrllanbrandao <yrllanbrandao@outlook.com>
2023-06-03 11:04:06 -0300

    refactor(routes): splitting the files routes.ts into three files

--- server/dist/routes/routes.js
+++ server/dist/routes/routes.js
@@ -1,60 +1,14 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const user_1 = __importDefault(require("../models/user"));
-const article_1 = __importDefault(require("../models/article"));
-const contact_1 = __importDefault(require("../models/contact"));
-const middleware_1 = __importDefault(require("../auth/middleware"));
+const user_1 = __importDefault(require("./user"));
+const article_1 = __importDefault(require("./article"));
+const contact_1 = __importDefault(require("./contact"));
 const Routes = express_1.default.Router();
-const middleware = new middleware_1.default();
-Routes.post("/authentication", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const user = new user_1.default();
-    user.login(req, res);
-}));
-Routes.post("/user", middleware.handle, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const user = new user_1.default();
-    user.createUser(req, res);
-}));
-// contact
-Routes.get("/contacts", middleware.handle, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const contact = new contact_1.default();
-    contact.findAll(req, res);
-}));
-Routes.post("/contact", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const contact = new contact_1.default();
-    contact.registerContact(req, res);
-}));
-// articles
-Routes.get("/articles", middleware.handle, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const article = new article_1.default();
-    article.getArticles(req, res);
-}));
-Routes.post("/article", middleware.handle, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const article = new article_1.default();
-    article.creatArticle(req, res);
-}));
-Routes.get("/article/:key", middleware.handle, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const article = new article_1.default();
-    article.getArticleByKey(req, res);
-}));
-Routes.delete("/article/:id", middleware.handle, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const article = new article_1.default();
-    article.deleteArticle(req, res);
-}));
-Routes.put("/article/:id", middleware.handle, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const article = new article_1.default();
-    article.updateArticle(req, res);
-}));
+Routes.use("/", user_1.default);
+Routes.use("/", contact_1.default);
+Routes.use("/", article_1.default);
 exports.default = Routes;
