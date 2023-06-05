4ff465b7d32396ddc112202e61d0bdacf0aea386
yrllanbrandao <yrllanbrandao@outlook.com>
2023-06-05 09:58:51 -0300

    test(contact): getById

---
+++ server/tests/contact/getById.test.ts
@@ -0,0 +1,31 @@
+import { it, describe, expect, beforeAll, afterAll } from '@jest/globals';
+import supertest, { Response } from 'supertest';
+import app from '../../server';
+
+
+
+describe("get contact by Id", () => {
+
+    beforeAll(() => {
+        process.env.NODE_ENV = 'test';
+    })
+
+    it("should return status 200", async()=>{
+        const res:Response = await supertest(app)
+        .get("/article/1");
+
+        expect(res.status).toBe(200);
+    })
+    it.only("should return status 404", async ()=>{
+        const res:Response = await supertest(app)
+        .get("/article/99999");
+
+        expect(res.status).toBe(404);
+    })
+
+    afterAll(() => {
+        process.env.NODE_ENV = 'development';
+    })
+    
+    
+    })
