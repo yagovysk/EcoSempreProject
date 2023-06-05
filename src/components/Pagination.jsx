52fccff6be12465aa885c67ea2173ace607e61b9
Raiane <raiane.oliveira404@gmail.com>
2023-06-05 18:08:46 -0300

    feat: json-server fake REST API for prototyping and mocking

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,17 +1,27 @@
 import { Icon } from "@iconify/react";
 import styles from "./Pagination.module.css";
 
-export function Pagination({ size, currentPage }) {
-  const sizePagesArr = Array.from(Array(size), (n, index) => index + 1);
+export function Pagination({
+  pageIndex,
+  onNextPage,
+  postsPerPage,
+  postsLength,
+}) {
+  const sizePagination = Math.ceil(postsLength / postsPerPage);
 
+  const sizePagesArr = Array.from(
+    Array(sizePagination),
+    (n, index) => index + 1
+  );
   return (
     <div className={styles.wrapper_pagination}>
-      {sizePagesArr.map((page) => (
+      {sizePagesArr.map((page, index) => (
         <button
-          key={page}
+          key={index}
           type="button"
+          onClick={() => onNextPage(index)}
           className={`${styles.btn_pagination} ${
-            page === currentPage && styles.active
+            page === pageIndex + 1 && styles.active
           }`}
         >
           {page}
@@ -19,6 +29,8 @@ export function Pagination({ size, currentPage }) {
       ))}
       <button
         type="button"
+        disabled={pageIndex + 1 >= sizePagination}
+        onClick={() => onNextPage(pageIndex + 1)}
         className={`${styles.btn_pagination} ${styles.btn_arrow} `}
       >
         <Icon icon="octicon:arrow-right-16" />
