544c95a8012dea530c57ea7c36361a272ceab5bb
yrllanbrandao <yrllanbrandao@outlook.com>
2023-07-03 14:04:46 -0300

    refactor(newsletter): new response status if the e-mail already exist

--- server/models/newsletter.ts
+++ server/models/newsletter.ts
@@ -6,47 +6,53 @@ import Static from "../static";
 class Newsletter {
     constructor() { }
     private currentDate: string = new Static().getCurrentDate();
-    private async verifyEmail(email:string)
-    {
-        const query:object | undefined = await Connection("newsletter").select("*").where({email});
+    private async verifyEmail(email: string) {
+        const query: object | undefined = await Connection("newsletter").select("*").where({ email });
 
-        if(query !== undefined)
-        {
+        if (query !== undefined) {
             return true;
         }
         return false;
     }
-    private validateEmail(email:string){
+    private validateEmail(email: string) {
         const pattern: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-        if(pattern.test(email))
-        {
+        if (pattern.test(email)) {
             return true;
         }
         return false;
     }
     public async registerEmail(req: Request, res: Response) {
         try {
-            
+
 
             const email: string = req.body;
 
-            const isValid:boolean = this.validateEmail(email);
+            const isValid: boolean = this.validateEmail(email);
+            const exist: boolean = await this.verifyEmail(email);
             if (isValid) {
-                await Connection("newsletter").insert({
-                    email,
-                    createdAt: this.currentDate,
-                    updatedAt: this.currentDate
-                })
-                res.sendStatus(201);
+
+
+                if (exist) {
+                    res.status(409).send("The e-mail already is registered!!!")
+                }
+                else {
+                    await Connection("newsletter").insert({
+                        email,
+                        createdAt: this.currentDate,
+                        updatedAt: this.currentDate
+                    })
+                    res.sendStatus(201);
+                }
             }
+
         }
         catch (error: any) {
             res.sendStatus(400);
 
         }
     }
- 
+
 }
 
 
