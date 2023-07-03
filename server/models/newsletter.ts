0f6d3c1571ec649ca82d9cc47ae55d0712266124
yrllanbrandao <yrllanbrandao@outlook.com>
2023-07-03 12:36:53 -0300

    feat(newsletter): validate email method

--- server/models/newsletter.ts
+++ server/models/newsletter.ts
@@ -6,12 +6,23 @@ import Static from "../static";
 class Newsletter {
     constructor() { }
     private currentDate: string = new Static().getCurrentDate();
+    private validateEmail(email:string){
+        const pattern: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if(pattern.test(email))
+        {
+            return true;
+        }
+        return false;
+    }
     public async registerEmail(req: Request, res: Response) {
         try {
-            const pattern: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            
 
             const email: string = req.body;
-            if (pattern.test(email)) {
+
+            const isValid:boolean = this.validateEmail(email);
+            if (isValid) {
                 await Connection("newsletter").insert({
                     email,
                     createdAt: this.currentDate,
