3a2026fd16c25cf9478562ab5df33931938271f6
yrllanbrandao <yrllanbrandao@outlook.com>
2023-07-15 18:45:56 -0300

    feat(categoryCollectionPoints): create category method

---
+++ server/models/collectionPoints.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from "express";
+
+import Connection from "../database/connection";
+import Static from "../static";
+
+
+// interface ICollectionPoints {
+//     name: string,
+//     address: string,
+//     cep: string
+//     category: number,
+//     state: string
+//     size: string
+// }
+
+
+
+// class collectionPoints{
+//     constructor(){}
+
+
+// }
+
+// export default collectionPoints;
+
+interface ICategoryCollectionPoints{
+    name: string,
+    createdAt?: string,
+    updatedAt?: string,
+
+}
+
+class CategoryCollectionPoints{
+    constructor(){}
+    private currentDate: string = new Static().getCurrentDate();
+
+    private async verifyCategoryByname(name:string)
+    {
+        const query:object | undefined =  await Connection("categoryCollectionPoints").select("*").where({name}).first();
+
+        if(query === undefined)
+        {
+            return false;
+        }
+        return true;
+    }
+    public async createCategory(req:Request, res:Response)
+    {
+        try{
+        const newCategory:ICategoryCollectionPoints = req.body;
+
+        const category: ICategoryCollectionPoints = {
+            ...newCategory,
+            createdAt: this.currentDate,
+            updatedAt: this.currentDate
+        }
+
+        const alreadyExist:boolean = await this.verifyCategoryByname(category.name);
+
+        if(alreadyExist)
+        {
+            res.sendStatus(409);
+        }
+        else{
+            await Connection("categoryCollectionPoints").insert(category);
+
+            res.sendStatus(201);
+        }
+        }
+        catch(error:any)
+        {
+            res.status(400).send(error.message);
+        }
+    }
+
+}
+
+export default CategoryCollectionPoints;
