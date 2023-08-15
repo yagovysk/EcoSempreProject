20a90bc19a54522164598bfc4d703f00e4b56f85
Raiane <raiane.oliveira404@gmail.com>
2023-08-15 17:24:58 -0300

    fix: calling API even when doesn't have a query search

--- src/pages/SearchResult/index.jsx
+++ src/pages/SearchResult/index.jsx
@@ -1,58 +1,63 @@
-import { Link, useLoaderData, useNavigation, Form } from "react-router-dom";
-import { useState } from "react";
-import { Icon } from "@iconify/react";
-import { HeaderSection } from "../../components/HeaderSection";
-import { AsideBlog } from "../../components/AsideBlog";
-import { Pagination } from "../../components/Pagination";
-import api from "../../api/posts";
-import "./SearchResult.css";
+import { Link, useLoaderData, useNavigation, Form } from 'react-router-dom'
+import { useState } from 'react'
+import { Icon } from '@iconify/react'
+import { HeaderSection } from '../../components/HeaderSection'
+import { AsideBlog } from '../../components/AsideBlog'
+import { Pagination } from '../../components/Pagination'
+import api from '../../api/posts'
+import './SearchResult.css'
 
 const linksMenu = [
   {
-    name: "Início",
-    path: "/",
+    name: 'Início',
+    path: '/',
   },
   {
-    name: "Busca",
+    name: 'Busca',
   },
-];
-const POSTS_PER_PAGE = 3;
+]
+const POSTS_PER_PAGE = 3
 
 export async function loader({ request }) {
-  const url = new URL(request.url);
-  const q = url.searchParams.get("q");
+  const url = new URL(request.url)
+  const q = url.searchParams.get('q')
+
+  if (!q) {
+    return { posts: [], q }
+  }
+
   const posts = await api
     .get(`/articles?title_like=${q}`)
     .then((response) => response.data)
     .catch((err) => {
-      throw new Response("", {
+      throw new Response('', {
         status: err.response.status,
         statusText: err.response.statusText,
-      });
-    });
+      })
+    })
 
-  return { posts, q };
+  return { posts, q }
 }
 
 export function SearchResult() {
-  const { posts, q } = useLoaderData();
-  const [pageIndex, setPageIndex] = useState(0);
+  const { posts, q } = useLoaderData()
+  const [pageIndex, setPageIndex] = useState(0)
 
-  const startIndex = pageIndex * POSTS_PER_PAGE;
-  const endIndex = startIndex + POSTS_PER_PAGE;
-  const postsPerPage = posts.slice(startIndex, endIndex);
+  const startIndex = pageIndex * POSTS_PER_PAGE
+  const endIndex = startIndex + POSTS_PER_PAGE
+  const postsPerPage = posts.slice(startIndex, endIndex)
 
   return (
     <main className={`main_search_result`}>
       <HeaderSection
         title="Resultados da busca"
         linksMenu={linksMenu}
-        className={"header"}
+        className={'header'}
       />
 
       <div className={`container_content container`}>
         {posts.length ? (
-          <div className={"posts_container"}>
+          <div className={'posts_container'}>
             {postsPerPage.map((post) => (
               <Card
                 key={post.id}
@@ -71,9 +76,9 @@ export function SearchResult() {
             />
           </div>
         ) : (
-          <section className={"error_wrapper"}>
-            <h2 className={"title"}>Nenhum Resultado Encontrado!</h2>
-            <p className={"content_error"}>
+          <section className={'error_wrapper'}>
+            <h2 className={'title'}>Nenhum Resultado Encontrado!</h2>
+            <p className={'content_error'}>
               Desculpe, não encontramos resultados para a sua busca. Mas não
               desista! Continue explorando nosso site para encontrar outras
               informações valiosas sobre reciclagem e sustentabilidade.
@@ -86,18 +91,18 @@ export function SearchResult() {
         <AsideBlog />
       </div>
     </main>
-  );
+  )
 }
 
 function FormSearch({ placeholder }) {
-  const [search, setSearch] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
-  const navigation = useNavigation();
+  const [search, setSearch] = useState('')
+  const [isTouched, setIsTouched] = useState(false)
+  const navigation = useNavigation()
 
   return (
     <Form
       role="search"
-      className={`search_form ${isTouched ? "active" : ""}`}
+      className={`search_form ${isTouched ? 'active' : ''}`}
       onFocus={() => setIsTouched(true)}
       onBlur={() => setIsTouched(false)}
     >
@@ -110,26 +115,26 @@ function FormSearch({ placeholder }) {
         className="search_input"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        disabled={navigation.state === "loading"}
+        disabled={navigation.state === 'loading'}
       />
     </Form>
-  );
+  )
 }
 
 function Card({ id, title, content, imgURL }) {
   return (
-    <article className={"card_wrapper"}>
-      <div className={"card_img_wrapper"}>
+    <article className={'card_wrapper'}>
+      <div className={'card_img_wrapper'}>
         <img src={imgURL} alt={title} />
       </div>
 
-      <section className={"card_texts_wrapper"}>
+      <section className={'card_texts_wrapper'}>
         <h2 className={`title title_card`}>{title}</h2>
-        <p className={"content"}>{content}</p>
-        <Link to={`/posts/${id}`} className={`link_more ${"card_link"}`}>
+        <p className={'content'}>{content}</p>
+        <Link to={`/posts/${id}`} className={`link_more ${'card_link'}`}>
           Saiba Mais
         </Link>
       </section>
     </article>
-  );
+  )
 }
