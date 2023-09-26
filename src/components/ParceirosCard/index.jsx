6149bac320a3661fd035378137f781f121091af7
Raiane <raiane.oliveira404@gmail.com>
2023-09-26 19:01:36 -0300

    fix: scrollbar and partners padding section

--- src/components/ParceirosCard/index.jsx
+++ src/components/ParceirosCard/index.jsx
@@ -6,7 +6,7 @@ export function ParceirosCard() {
   return (
     <div className={`${styles.parceirosCard} min-[1760px]:justify-center`}>
       {partners.map((partner) => (
-        <a key={partner} href="#">
+        <a className={styles.partner_wrapper} key={partner} href="#">
           <img
             loading="lazy"
             className={styles.parceiro}
